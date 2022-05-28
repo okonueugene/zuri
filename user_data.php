3ce66236c8add15c06b97d52b6dac4e7594cd385
okonueugene <97091138+okonueugene@users.noreply.github.com>
2022-05-28 06:55:55 +0300

    Update user_data.php

--- user_data.php
+++ user_data.php
@@ -1,12 +1,7 @@
 <?php
 
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$dob = $_POST['dob'];
-$gender = $_POST['gender'];
-$country = $_POST['country'];
-
+print_r($_POST);
 
 $output = implode(",", $_POST);
 
@@ -14,12 +9,6 @@ $filename = "./userdata.csv";
 $handle = fopen($filename, "w");
 fwrite($handle, $output);
 fclose($handle);
-
-echo "NAME: " . $name . "<br>";
-echo "EMAIL: " . $email . "<br>";
-echo "DATE OF BIRTH: " . $dob . "<br>";
-echo "GENDER: " . $gender . "<br>";
-echo "COUNTRY: " . $country . "<br>";
 
 
 ?>
